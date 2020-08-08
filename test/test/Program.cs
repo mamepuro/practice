176efed9a67f476e7d87f72d8025a37ad34de261
mamepuro <s1911320@coins.tsukuba.ac.jp>
2020-08-08 13:58:14 +0900

    a

---
+++ test/test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //string test1 = ;
+            //var test2 = "権兵衛";
+            Object obj = 100;
+            switch(obj)
+            {
+                #region case文書きまくり
+                case int i when i >= 70:
+                    Console.WriteLine("優");
+                    break;
+                case int i:
+                    Console.WriteLine("数値です");
+                    break;
+                default:
+                    Console.WriteLine("意図しない型です");
+                    break;
+                #endregion
+            }
+
+        }
+    }
+}
